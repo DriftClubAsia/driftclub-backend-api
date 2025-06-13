d9f229cba46776d88da16616ec98e8412eb2fdfb
DriftClubAsia <oliver@driftclub.asia>
2025-06-13 23:07:03 +0700

    Update route.js

--- app/api/recommendations/route.js
+++ app/api/recommendations/route.js
@@ -40,7 +40,7 @@ export async function GET(request) {
         type: process.env.GOOGLE_TYPE,
         project_id: process.env.GOOGLE_PROJECT_ID,
         private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\n/g, '\n'),
+        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
         client_id: process.env.GOOGLE_CLIENT_ID,
         client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL
